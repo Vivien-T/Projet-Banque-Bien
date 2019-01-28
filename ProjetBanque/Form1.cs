177d0f9b385179c9b6ca8b3307859e81223ef29b
Vivien-T <44870403+Vivien-T@users.noreply.github.com>
2019-01-28 13:56:56 +0100

    Update Form1.cs

--- ProjetBanque/Form1.cs
+++ ProjetBanque/Form1.cs
@@ -18,22 +18,6 @@ namespace ProjetBanque
             creerClient();
         }
 
-        Client creerClient()
-        {
-            Client client = new Client();
-
-            Console.Write("Saisir le nom de famille du client : ");
-            client.nom = Console.ReadLine();
-
-            Console.Write("Saisir le prénom du client : ");
-            client.prenom = Console.ReadLine();
-
-            Console.Write("Saisir la date de naissance du client : ");
-            client.DateNaissance = Console.Readline();
-
-            Console.WriteLine("ntm");
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
