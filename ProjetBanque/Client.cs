a8b776ea0f8da377307ee39d40c3b64850401115
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-01-28 14:33:15 +0100

    uyes

--- ProjetBanque/Client.cs
+++ ProjetBanque/Client.cs
@@ -8,7 +8,7 @@ namespace ProjetBanque
 {
     class Client
     {
-    public int IdClient;
+        public int IdClient;
         public string nom;
         public string prenom;
         public DateTime DateNaissance;
