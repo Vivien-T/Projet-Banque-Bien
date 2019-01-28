efdf4251924590b8f6d69a53ae2170b994e6f190
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-01-28 12:16:09 +0100

    Form

--- ProjetBanque/Form1.Designer.cs
+++ ProjetBanque/Form1.Designer.cs
@@ -28,21 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Bouton1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Bouton1
+            // 
+            this.Bouton1.AccessibleName = "";
+            this.Bouton1.Location = new System.Drawing.Point(510, 300);
+            this.Bouton1.Name = "Bouton1";
+            this.Bouton1.Size = new System.Drawing.Size(196, 61);
+            this.Bouton1.TabIndex = 0;
+            this.Bouton1.Text = "Banque";
+            this.Bouton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenue sur votre Banque";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Bouton1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Bouton1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
