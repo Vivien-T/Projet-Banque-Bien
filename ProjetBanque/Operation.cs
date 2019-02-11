8a9ba87047ca6466f6b5bf138be5f5be88cc77d6
ljoub <45665062+ljoub@users.noreply.github.com>
2019-02-11 11:25:22 +0100

    tamerelaputevivien

--- ProjetBanque/Operation.cs
+++ ProjetBanque/Operation.cs
@@ -8,10 +8,40 @@ namespace ProjetBanque
 {
     class Operation
     {
-        private int IdOperation;
-        private double date;
+        private int idoperation;
+        private DateTime date;
         private double montant;
         private int libelle;
         private bool debit;
+
+        public int IdOperation
+        {
+            get { return idoperation; }
+            set { idoperation = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public double Montant
+        {
+            get { return montant; }
+            set { montant = value; }
+        }
+
+        public int Libelle
+        {
+            get { return libelle; }
+            set { libelle = value; }
+        }
+
+        public bool Debit
+        {
+            get { return debit; }
+            set { debit = value; }
+        }
     }
 }
