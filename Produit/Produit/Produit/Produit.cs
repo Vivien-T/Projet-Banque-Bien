4b0d7e1aabc95b1d25a077a9b043c92b1473a5da
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-02-11 11:07:52 +0100

    Revert "grrrgq"
    This reverts commit 0511a8995cd985dee51c1f5e987dc28739906779, reversing
    changes made to 4afdceb9bd3b29d18e35e9d19c871af07c728654.

---
+++ Produit/Produit/Produit/Produit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBanque
+{
+    class Produit
+    {
+        #region Attributs
+        private int id;
+        #endregion
+
+        #region Propiétés
+        public int ID
+        {
+            get { return ID; }
+            set { id = value; }
+        }
+
+        public string Libelle { get; set; }
+        #endregion
+
+        public string Description
+        {
+            get { return string.Format("N°{0} - {1}", id, Libelle); }
+        }
+    }
+}
