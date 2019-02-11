6177a8288883a0c4094c3748df38ee3ef5859788
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-02-11 09:16:10 +0100

    Probelem

---
+++ Produit/Produit/Produit/Magasin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetBanque
+{
+    class Magasin
+    {
+        #region Attributs
+        private List<Produit> produits = new List<Produit>();
+        #endregion
+
+        public void AjouterProduit(Produit produit)
+        {
+            produits.Add(produit);
+        }
+
+        public Produit RechercherProduit(int id)
+        {
+
+        }
+    }
+}
