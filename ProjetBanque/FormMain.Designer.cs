1df82406046afb06c567a70e028c96f8d66b066b
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-02-24 23:49:22 +0100

    correction_orthographe_niveau_cp(accueil)

--- ProjetBanque/FormMain.Designer.cs
+++ ProjetBanque/FormMain.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Texte_Acceuil = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Texte_Acceuil
+            // 
+            this.Texte_Acceuil.AutoSize = true;
+            this.Texte_Acceuil.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.Texte_Acceuil.Location = new System.Drawing.Point(53, 13);
+            this.Texte_Acceuil.Name = "Texte_Acceuil";
+            this.Texte_Acceuil.Size = new System.Drawing.Size(102, 31);
+            this.Texte_Acceuil.TabIndex = 0;
+            this.Texte_Acceuil.Text = "Accueil";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Texte_Acceuil);
             this.Name = "FormMain";
             this.Text = "FormMain";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Texte_Acceuil;
     }
 }
