99e4a292843145329bb0a1fe76def047aaebfd3c
Tino Féraud Dentinger <feraud.tino@gmail.com>
2019-03-18 11:40:44 +0100

    jtj

--- ProjetBanque/FormMain.Designer.cs
+++ ProjetBanque/FormMain.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.Texte_Acceuil = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Texte_Acceuil
@@ -41,11 +42,22 @@
             this.Texte_Acceuil.TabIndex = 0;
             this.Texte_Acceuil.Text = "Accueil";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Texte_Acceuil);
             this.Name = "FormMain";
             this.Text = "FormMain";
@@ -58,5 +70,6 @@
         #endregion
 
         private System.Windows.Forms.Label Texte_Acceuil;
+        private System.Windows.Forms.Label label1;
     }
 }
